9e81c7a70e1909f3d888313cbe7fa9920444745b
ohmin839 <ohmin839.echoda.resident@gmail.com>
2023-07-29 08:07:49 +0000

    Added jybuben-batch project

---
+++ java/jybuben-batch/README.md
@@ -0,0 +1,1 @@
+# jybuben-batch
---
+++ java/jybuben-batch/src/main/java/com/ohmin839/jybuben/batch/JybubenBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ohmin839.jybuben.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JybubenBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JybubenBatchApplication.class, args);
+	}
+
+}
